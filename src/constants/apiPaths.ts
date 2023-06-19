86c402b0c3fc77996ff28d7ccca609ef00077b24
Andrei Liaukou <andrei_liaukou@epam.com>
2023-06-19 14:25:41 +0200

    Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://0teqah93ch.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://czqk2hrt64.execute-api.eu-west-1.amazonaws.com/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
